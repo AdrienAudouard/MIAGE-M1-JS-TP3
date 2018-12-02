314bcdf6a878037e3d5ec8cec21732e3e6afeba6
Adrien Audouard <adrien.audouard@gmail.com>
2018-12-02 16:11:49 +0100

    Initial commit

---
+++ client/js/script.js
@@ -0,0 +1,107 @@
+window.onload = init;
+
+function init() {
+    new Vue({
+        el: "#app",
+        data: {
+            restaurants: [{
+                    nom: 'café de Paris',
+                    cuisine: 'Française'
+                },
+                {
+                    nom: 'Sun City Café',
+                    cuisine: 'Américaine'
+                }
+            ],
+            nom: '',
+            cuisine: '',
+            nbRestaurants:0,
+            page:0,
+            pagesize:10,
+            name:""
+        },
+        mounted() {
+            console.log('Oui');
+            console.log("AVANT AFFICHAGE");
+            this.getRestaurantsFromServer();
+        },
+        methods: {
+            getRestaurantsFromServer() {
+                if (this.page < 0) {
+                    this.page = 0;
+                }
+                let url = "http://localhost:8080/api/restaurants?page=" +
+                    this.page + "&pagesize=" +
+                    this.pagesize + "&name=" +
+                    this.name;
+
+                fetch(url)
+                    .then((reponseJSON) => {
+                        reponseJSON.json()
+                            .then((reponseJS) => {
+                                this.restaurants = reponseJS.data;
+                                this.nbRestaurants = reponseJS.count;
+                                console.log(reponseJS.msg);
+                            });
+                    })
+                    .catch(function (err) {
+                        console.log(err);
+                    });
+            },
+            supprimerRestaurant(index) {
+                this.restaurants.splice(index, 1);
+            },
+            ajouterRestaurant(event) {
+                // eviter le comportement par defaut
+                event.preventDefault();
+
+                // Récupération du contenu du formulaire pour envoi en AJAX au serveur
+                // 1 - on récupère le formulaire
+                let form = event.target;
+
+                // 2 - on récupère le contenu du formulaire
+                let dataFormulaire = new FormData(form);
+
+                // 3 - on envoie une requête POST pour insertion sur le serveur
+                let url = "http://localhost:8080/api/restaurants";
+
+                fetch(url, {
+                        method: "POST",
+                        body: dataFormulaire
+                    })
+                    .then((reponseJSON) => {
+                        reponseJSON.json()
+                            .then((reponseJS) => {
+                                console.log(reponseJS.msg);
+                                // On re-affiche les restaurants
+                                this.getRestaurantsFromServer();
+                            });
+                    })
+                    .catch(function (err) {
+                        console.log(err);
+                    });
+                
+                this.nom = "";
+                this.cuisine = "";
+            },
+            getColor(index) {
+                return (index % 2) ? 'lightBlue' : 'pink';
+            },
+            premierePage() {
+                this.page++;
+                this.getRestaurantsFromServer();
+            },
+            pageSuivante() {
+                this.page++;
+                this.getRestaurantsFromServer();
+            },
+            pagePrecedente() {
+                this.page--;
+                this.getRestaurantsFromServer();
+            },
+            chercherRestaurants: _.debounce(function () {
+                this.getRestaurantsFromServer();
+            }, 500)
+        }
+    })
+}
